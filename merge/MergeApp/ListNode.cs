9daf9a6edf558eedf6b9680f6ac8afb8bfae4ce0
Abdulrahman Akkash <77051276+Abdulrahman-Akkash@users.noreply.github.com>
2025-05-21 10:04:07 +0300

    Add files via upload

---
+++ merge/MergeApp/ListNode.cs
@@ -0,0 +1,76 @@
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+    public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+    {
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null)
+        {
+            if (l1.val < l2.val)
+            {
+                tail.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+    public static ListNode MergeKLists(ListNode[] lists)
+    {
+        if (lists == null || lists.Length == 0) return null;
+        return MergeKListsHelper(lists, 0, lists.Length - 1);
+
+    }
+
+    private  static ListNode MergeKListsHelper(ListNode[] lists, int left, int right)
+    {
+        if (left == right) return lists[left];
+
+        int mid = (left + right) / 2;
+        ListNode l1 = MergeKListsHelper(lists, left, mid);
+        ListNode l2 = MergeKListsHelper(lists, mid + 1, right);
+        return MergeTwoLists(l1, l2);
+
+    }
+    public static ListNode BuildList(int[] values)
+    {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        foreach (int val in values)
+        {
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+        return dummy.next;
+
+    }
+
+    public static void PrintList(ListNode head)
+    {
+        while (head != null)
+        {
+            Console.Write(head.val + " -> ");
+            head = head.next;
+        }
+        Console.WriteLine("null");
+
+    }
+
+
+}
---
+++ merge/MergeApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// var lists = new ListNode[] {
+//         ListNode.BuildList(new int[] {1, 4, 5}),
+//         ListNode.BuildList(new int[] {1, 3, 4}),
+//         ListNode.BuildList(new int[] {2, 6})
+//     };
+
+// var merged = ListNode.MergeKLists(lists);
+// ListNode.PrintList(merged);  // Output: 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> 5 -> 6 -> null
+var lists = new ListNode[]{
+    ListNode.BuildList(new int[]{ }),
+    ListNode.BuildList(new int[] {1,3,5}),
+    ListNode.BuildList(new int[]{2,4,99})
+};
+var merged = ListNode.MergeKLists(lists);
+ListNode.PrintList(merged);

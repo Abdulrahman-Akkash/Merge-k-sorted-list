5a4a183a4627f9b318111501c40ccbb91e9e5453
Abdulrahman Akkash <77051276+Abdulrahman-Akkash@users.noreply.github.com>
2025-06-01 23:05:17 +0300

    Delete MergeApp directory

--- MergeApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-var lists = new ListNode[] {
-        ListNode.BuildList(new int[] {1, 4, 5}),
-        ListNode.BuildList(new int[] {1, 3, 4}),
-        ListNode.BuildList(new int[] {2, 6})
-    };
-
-var merged = ListNode.MergeKLists(lists);
-ListNode.PrintList(merged);  // Output: 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> 5 -> 6 -> null

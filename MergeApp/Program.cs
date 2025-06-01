d89c5b1e84f0219eee701556c34aa1c02a9ca65b
Abdulrahman Akkash <77051276+Abdulrahman-Akkash@users.noreply.github.com>
2025-06-01 23:01:01 +0300

    Add MergeApp project files and binaries for net8.0 and net9.0

---
+++ MergeApp/Program.cs
@@ -0,0 +1,9 @@
+﻿
+var lists = new ListNode[] {
+        ListNode.BuildList(new int[] {1, 4, 5}),
+        ListNode.BuildList(new int[] {1, 3, 4}),
+        ListNode.BuildList(new int[] {2, 6})
+    };
+
+var merged = ListNode.MergeKLists(lists);
+ListNode.PrintList(merged);  // Output: 1 -> 1 -> 2 -> 3 -> 4 -> 4 -> 5 -> 6 -> null
